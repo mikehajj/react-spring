ea4feb32156fc97def05631fa968899ecb07fa6d
mikehajj <mike.hajj@gmail.com>
2022-02-15 17:32:02 +0200

    minor modification to the java start command.

--- k8s/BE/script/start.sh
+++ k8s/BE/script/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Starting the java applictaion ..."
-java -jar target/react-and-spring-data-rest-*.jar
+# send it to the background and resume with the next instruction
+(java -jar target/react-and-spring-data-rest-*.jar) &
 
 echo "Starting Nginx web server ...."
 nginx
